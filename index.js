5e3bba66248dbe198a721760f8f785aa184dab4c
AkshayMinde <akshayminde6825@gmail.com>
2023-07-26 02:19:02 +0530

    homepage updated

--- index.js
+++ index.js
@@ -94,6 +94,6 @@ app.use(quesRoutes);
 
 
 const port = process.env.PORT || 3000;
-app.listen(3000, (req, res)=>{
-    console.log(`Server is working on 3000`);
+app.listen(port, (req, res)=>{
+    console.log(`Server is working `);
 })
